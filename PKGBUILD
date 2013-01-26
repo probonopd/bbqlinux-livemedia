d36fede1b03c2712b32e8e46b7359e5d2b0c3158
codeworkx <codeworkx@cyanogenmod.org>
2013-01-26 09:11:55 +0000

    initial commit

---
+++ PKGBUILD
@@ -0,0 +1,21 @@
+# Maintainer: Daniel Hillenbrand <codeworkx@bbqlinux.org>
+
+pkgname=bbqlinux-livemedia
+pkgver=0.0.1
+pkgrel=3
+pkgdesc="Tools for the BBQLinux live media"
+arch=('any')
+url="https://github.com/bbqlinux/bbqlinux-livemedia"
+license=('GPL')
+depends=('python2' 'qt' 'python2-pyqt')
+
+package() {
+  cd "$pkgdir"
+
+  install -Dm755 "$srcdir/usr/bin/bbqlinux-greeter" usr/bin/bbqlinux-greeter
+  
+  cp -R "$srcdir/etc" etc
+  cp -R "$srcdir/usr/lib" usr/lib
+  cp -R "$srcdir/usr/share" usr/share
+  
+}
---
+++ src/usr/lib/bbqlinux-greeter/ui/qt_interface.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys
+sys.path.append('/usr/lib/bbqlinux-greeter')
+import os
+import string
+
+from greeter import GreeterEngine
+
+from PyQt4 import QtGui, QtCore, uic
+
+class GreeterWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+
+        QtGui.QMainWindow.__init__(self)
+        self.ui = uic.loadUi('/usr/share/bbqlinux-greeter/qt_interface.ui')
+
+        # Set window title
+        self.ui.setWindowTitle("Welcome to BBQLinux")
+        self.ui.setWindowIcon(QtGui.QIcon('/usr/share/bbqlinux/icons/bbqlinux_icon_blue_32x32.png'))
+
+        # Show the window
+        self.ui.show()
+        
+        # Move main window to center
+        qr = self.ui.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.ui.move(qr.topLeft())
+        
+        # Connect the buttons
+        self.connect(self.ui.button_exit, QtCore.SIGNAL("clicked()"), QtGui.qApp, QtCore.SLOT("quit()"))
